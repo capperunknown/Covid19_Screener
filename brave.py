0711001ee74f1739108a11c48f6941d95b13938e
capperunknown <79781802+capperunknown@users.noreply.github.com>
2022-01-02 20:06:53 -0500

    Code does the same thing as main.py except launches brave browser instead of chrome browser

--- brave.py
+++ brave.py
@@ -53,18 +53,16 @@ def launchGmail(browser):
 
     browser.get('https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
     email = browser.find_element_by_xpath('//*[@id="identifierId"]')
-    email.send_keys('abdul.hafeezm02')
+    email.send_keys('REDACTED')
     email.send_keys(Keys.ENTER)
     time.sleep(2)
     password = browser.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
-    password.send_keys('')
+    password.send_keys('REDACTED')
     password.send_keys(Keys.ENTER)
     time.sleep(4)
 
     browser.find_element_by_xpath('//*[@id=":kr"]/div/div').click()
     time.sleep(2)
-    #receiver = browser.find_element_by_xpath('//*[@id=":qe"]').click()
-    #receiver.send_keys('mohammadadil301@gmail.com')
     
     
     subjectLine = browser.find_element_by_xpath('//*[@id=":pn"]').click()
@@ -76,7 +74,7 @@ def launchGmail(browser):
     browser.find_element_by_xpath('//*[@id=":pd"]').click()
 
 
-driverPath = '/Users/adilmohammad/Desktop/VS_CODE/chromedriver'
+driverPath = '/Users/REDACTED/Desktop/VS_CODE/chromedriver'
 binaryPath = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
 options = webdriver.ChromeOptions()
 options.binary_location = binaryPath
